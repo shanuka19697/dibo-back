a30e042b479438ffe3d208772679b5c7ff3420e1
shanuka_lakshan <110249936+shanuka19697@users.noreply.github.com>
2026-03-02 08:31:01 +0530

    Configure CORS with specific options

--- server.js
+++ server.js
@@ -8,7 +8,10 @@ import StData from './models/sData.model.js';
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods:["GET","POST","PUT","DELETE"]
+}));
 app.use(express.json());
 
 // Connect to Database
